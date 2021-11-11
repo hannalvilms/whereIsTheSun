74a04af4a661c3e900e67760df1764f781fe8a3d
hannalvilms <hannaliisavilms@gmail.com>
2021-11-11 12:43:53 +0200

    Updates

---
+++ precache-manifest.5f2df10b2102a60feeb1759b8e692a32.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96c4ea82e0f50b48f2b6836d2b1dbee4",
+    "url": "/whereIsTheSun/index.html"
+  },
+  {
+    "revision": "fdff20a7a30aba3f62a6",
+    "url": "/whereIsTheSun/static/css/main.e3502824.chunk.css"
+  },
+  {
+    "revision": "57ddaba643ea13a712ca",
+    "url": "/whereIsTheSun/static/js/2.a82fcae8.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/whereIsTheSun/static/js/2.a82fcae8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a52242dc6835eda2f8c",
+    "url": "/whereIsTheSun/static/js/3.96607391.chunk.js"
+  },
+  {
+    "revision": "fdff20a7a30aba3f62a6",
+    "url": "/whereIsTheSun/static/js/main.4f59ebe9.chunk.js"
+  },
+  {
+    "revision": "9aa301cc8eea7a31ae55",
+    "url": "/whereIsTheSun/static/js/runtime-main.aeb4eddd.js"
+  },
+  {
+    "revision": "2993703e4343f8ecadbccaaa2b5ee117",
+    "url": "/whereIsTheSun/static/media/cloudy3.2993703e.jpg"
+  },
+  {
+    "revision": "a9b1d27cafb10f600761675009a9c569",
+    "url": "/whereIsTheSun/static/media/fog5.a9b1d27c.jpg"
+  },
+  {
+    "revision": "6b10f880b610afedd82fe914ba806cab",
+    "url": "/whereIsTheSun/static/media/leaves.6b10f880.jpg"
+  },
+  {
+    "revision": "cac4037de3dbf434ab3e90502f642abc",
+    "url": "/whereIsTheSun/static/media/rain.cac4037d.jpg"
+  },
+  {
+    "revision": "09d7ae65701e27c3e6c1d9bedcb41b58",
+    "url": "/whereIsTheSun/static/media/sand.09d7ae65.jpg"
+  },
+  {
+    "revision": "e5a62d6ce7c1c128619cd0a72ad2603a",
+    "url": "/whereIsTheSun/static/media/snow2.e5a62d6c.jpg"
+  },
+  {
+    "revision": "d3f195332411f4fb07571d7a09bfe142",
+    "url": "/whereIsTheSun/static/media/thunder.d3f19533.jpg"
+  }
+]);
